969acfd57bce8b299f6fa179acb061c4014682a6
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-05-22 23:19:43 +0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Cek Username API</title>
+	<meta name="description" content="Cek Username Page" />
+</svelte:head>
+
+<section>
+	<h1 class="text-3xl md:text-5xl font-bold mt-28 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-pink-400 to-violet-500">
+		Cek Username API
+	</h1>
+
+</section>
+
+<style>
+	
+</style>
