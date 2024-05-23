eb6327bf1c6aa932a22af9a7a7c375a905f22679
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-05-23 20:33:33 +0700

    feat(page): home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,17 @@
 	<meta name="description" content="Cek Username Page" />
 </svelte:head>
 
-<section>
-	<h1 class="text-3xl md:text-5xl font-bold mt-28 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-pink-400 to-violet-500">
+<section class="mt-28">
+	<div class="text-center">
+	<h1 class=" text-3xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-pink-400 to-violet-500">
 		Cek Username API
 	</h1>
-
+	<h6 class="max-w-2xl mx-auto text-sm font-bold pt-4 self-center content-center">
+		Layanan API untuk validasi username beberapa layanan digital,
+		<br> seperti uid game, e-money, rekening bank,
+		pelanggan listrik, dan lainnya.
+	</h6>
+	</div>
 </section>
 
 <style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -22,8 +22,8 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/docs">Docs</a>
+			<li aria-current={$page.url.pathname === '/docs' ? 'page' : undefined}>
+				<a href="/Docs">Documentation</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
