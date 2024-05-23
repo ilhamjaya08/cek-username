32c9ccfa4d3049a9ee63f0d5483c83c8449872f3
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-05-23 23:21:45 +0700

    feat(card): api cards+crud

--- src/lib/core/ApiData.js
+++ src/lib/core/ApiData.js
@@ -1,30 +1,34 @@
-const baseUrl = () => {
-    if(process.env.NODE_ENV === 'production') {
-        return 'https://cek-username.vercel.app/api/'
-    } else {
-        return 'http://localhost:3000/api/'
-    }
-}
 
-export const ApiData = [
+
+const ApiData = [
     {
         id: 1,
         name: 'Game Nickname Validator',
-        url: baseUrl + 'game',
+        desc: '',
+        url: 'https://localhost:3000/api/' + 'game',
+        banner: "https://telegra.ph/file/839443d270c6ee419dad4.jpg"
     },
     {
         id: 2,
         name: 'E-Wallet Username Validator',
-        url: baseUrl + 'e-wallet',
+        desc: '',
+        url: 'https://localhost:3000/api/' + 'e-wallet',
+        banner: "https://console.kr-asia.com/wp-content/uploads/2020/07/Indonesia_mobile_wallet.png"
     },
     {
         id: 3,
         name: 'Rekening Bank Checker',
-        url: baseUrl + 'bank',
+        desc: '',
+        url: 'https://localhost:3000/api/' + 'bank',
+        banner: "https://th.bing.com/th/id/OIP.rftgwUfK7VYtfLiLGZ_KBAHaE8?rs=1&pid=ImgDetMain"
     },
     {
         id: 4,
         name: 'Cek Nama Pelanggan Listrik',
-        url: baseUrl + 'pln',
+        desc: '',
+        url: 'https://localhost:3000/api/' + 'pln',
+        banner: "https://cdn-brilio-net.akamaized.net/news/2022/06/14/231356/5-cara-cek-nomor-token-listrik-via-online-bisa-lewat-pln-mobile-220614i.jpg"
     }
 ]
+
+export default ApiData
