820353b440b4db2c29a94126c6d86daa0102323d
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-06-10 17:04:39 +0800

    fix: commonjs error on vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
-	import { inject } from '@vercel/analytics/*';
+	import { inject } from '@vercel/analytics';
 	inject()
 </script>
 
