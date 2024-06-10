d6606ad83406867e3123e1b83e380c0d2257f4e3
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-06-11 00:16:10 +0800

    feat: analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,16 @@
 	<main>
 		<slot />
 	</main>
-
+	<script type="text/javascript">
+		atOptions = {
+			'key' : '55ef8bfb6b7748db3f21080f7faaa22b',
+			'format' : 'iframe',
+			'height' : 60,
+			'width' : 468,
+			'params' : {}
+		};
+	</script>
+	<script type="text/javascript" src="//www.topcreativeformat.com/55ef8bfb6b7748db3f21080f7faaa22b/invoke.js"></script>
 	<footer>
 		<p>Made with ❤️ by ilhamjaya08</p>
 	</footer>
