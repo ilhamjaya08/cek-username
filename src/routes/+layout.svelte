30b3508306b4c46b8302afac572ab99e3c94aaf8
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-06-10 17:02:22 +0800

    feat: tawk.to live chat admin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,20 @@
 	<footer>
 		<p>Made with ❤️ by ilhamjaya08</p>
 	</footer>
+
+	<!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+	var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+	(function(){
+	var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+	s1.async=true;
+	s1.src='https://embed.tawk.to/6666c07f981b6c56477b7cef/1i00lvt0o';
+	s1.charset='UTF-8';
+	s1.setAttribute('crossorigin','*');
+	s0.parentNode.insertBefore(s1,s0);
+	})();
+	</script>
+	<!--End of Tawk.to Script-->
 </div>
 
 <style>
